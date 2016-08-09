ee1b51991481976394224a9e4537d759657eba21
qus <qus1225@gmail.com>
2016-08-09 20:50:58 +0900

    오프라인 서명인원 입력시 비밀번호를 요청합니다

--- app/controllers/offline_petitions_controller.rb
+++ app/controllers/offline_petitions_controller.rb
@@ -1,13 +1,17 @@
 class OfflinePetitionsController < ApplicationController
   def create
     @offline_petition = OfflinePetition.new(offline_petition_params)
-    @offline_petition.save
+    check_password(@offline_petition.pass_word) ? @offline_petition.save : flash[:notice] = '비밀번호가 틀렸습니다'
     redirect_to admin_path
   end
 
   private
 
   def offline_petition_params
-    params.require(:offline_petition).permit(:offline_count)
+    params.require(:offline_petition).permit(:offline_count, :pass_word)
+  end
+
+  def check_password(pass_word)
+    pass_word == '1dkdlznq@#'
   end
 end
--- app/models/offline_petition.rb
+++ app/models/offline_petition.rb
@@ -1,3 +1,3 @@
 class OfflinePetition < ActiveRecord::Base
-
+  attr_accessor :pass_word
 end
